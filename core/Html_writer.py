ce67530e62d086885a0665b6ade9d285114b679d
Fellzo <gely902@gmail.ru>
2016-04-11 22:11:01 +0300

    Add comments

--- core/Html_writer.py
+++ core/Html_writer.py
@@ -4,6 +4,10 @@
 """
 import core.Logs as Logs
 
+"""
+    Формирует строку вида <%tag_name% attrs>content</%tag_name%>
+"""
+
 
 def make_tag(tag, contain, attrib=""):
     return "<%s %s>\n%s\n</%s>\n" % (tag, attrib, contain, tag)
@@ -15,25 +19,27 @@ def write_html(output_data, configs):
     sort = sorted(output_data, key=lambda x: output_data[x])[::-1]  # Сортировка по количеству содержательных тегов
     for post in sort:  # Создание списка для вывода постов
         output_sorted.append((post, output_data[post]))
-    html = open(configs["output_file_name"], "w")
+    html = open(configs["output_file_name"], "w")  # файл с версткой
     link = "http://%s.stackexchange.com/questions/" % configs["prefix"]
-    limit = min(int(configs['output_limit']), len(output_sorted))
-    out = make_tag("h2", "Parsing site: %s" % configs["prefix"] + ".stackexchange.com")
-    out += make_tag("tr", make_tag("td", "#") + make_tag("td", "Links") + make_tag("td", "Meaningful tags"))  # Верстка
+    limit = min(int(configs['output_limit']), len(output_sorted))  # Кол-во выводимых постов
+    out = make_tag("h2", "Parsing site: %s" % configs["prefix"] + ".stackexchange.com")  # Создание заголовка таблицы
+    out += make_tag("tr", make_tag("td", "#") + make_tag("td", "Id") +
+                    make_tag("td", "Links") + make_tag("td", "Meaningful tags"))    # Шапка таблицы
     for i in range(limit):
-        post_link = (link + "%s/") % output_sorted[i][0]
-        out += make_tag("tr", make_tag("td", str(i + 1)) + make_tag("td", make_tag("a", "Link",
-                                                                                   attrib="href='%s' target='_blank'" % post_link)) + make_tag(
-            "td", str(
-                output_sorted[i][1])))
-    out = make_tag("table", out, attrib="class='bordered'")
-    out = make_tag("body", out)
+        post_link = (link + "%s/") % output_sorted[i][0]    # Ссылка на пост
+        # Генерация строки таблицы вида Номер Id Link Кол-во содержательный тегов в посте
+        out += make_tag("tr", make_tag("td", str(i + 1)) + make_tag("td", output_sorted[i][0]) +
+                        make_tag("td", make_tag("a", "Link", attrib="href='%s' target='_blank'" % post_link)) +
+                        make_tag("td", str(output_sorted[i][1])))   # строка таблицы с информацией о посте
+    out = make_tag("table", out, attrib="class='bordered'")     # Добавление тега таблицы
+    out = make_tag("body", out)     # Обертка таблицы в тег body
     css = ""
-    if configs["css"]:
+    if configs["css"]:  # Если css включен в настройках проверяется способ привязки к файлу
         if configs["css_type"] == "inline":
             css = make_tag("style", configs['style'])
         else:
             css = make_tag("link", "", attrib="type='text/css' rel='stylesheet' href='%s'" % configs['css_file_name'])
+    # Генерация содержания head тега
     head = make_tag("head", make_tag("meta", "", attrib="charset='utf-8'") + make_tag("title", configs['title']) + css)
     out = make_tag("html", (head + out).replace("\n\n", "\n"))
     html.writelines(out)
