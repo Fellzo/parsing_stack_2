7c0eb56886b44a7abb84de8c377dc9437a626d8e
Fellzo <gely902@gmail.ru>
2016-04-12 12:53:40 +0300

    Some fixes and result

--- Main.py
+++ Main.py
@@ -5,9 +5,12 @@ import core.Config as Config
 import core.Html_writer as Html
 
 
-output_data = {}
-Logs.add_log(Config.SEPARATOR + "Program started.")
-configs = Config.load_config()
-output_data[configs[0]['prefix']] = Parser.parse(configs[1], configs[0])    # Сохранение данных для каждого сайта
-Html.write_html(output_data[configs[0]['prefix']], configs[0])
-Logs.add_log("Program finished. Time: " + Time_lib.time_from_start() + Config.SEPARATOR)
+try:
+    output_data = {}
+    Logs.add_log(Config.SEPARATOR + "Program started.")
+    configs = Config.load_config()
+    output_data[configs[0]["prefix"]] = Parser.parse(configs[1], configs[0])    # Сохранение данных для каждого сайта
+    Html.write_html(output_data[configs[0]["prefix"]], configs[0])
+    Logs.add_log("Program finished. Time: " + Time_lib.time_from_start() + Config.SEPARATOR)
+except:
+    Logs.add_log("Unknown error.")
